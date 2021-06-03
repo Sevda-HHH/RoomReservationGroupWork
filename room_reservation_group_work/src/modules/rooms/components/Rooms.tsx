0a41bf89d118d8eac1789abccebb882e48bc52f5
Sevda-HHH <SevdaMh@code.edu.az>
2021-06-03 10:05:05 +0400

    RoomsPage Ul is added

---
+++ room_reservation_group_work/src/modules/rooms/components/Rooms.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RoomsSvg } from './RoomsSvg'
+
+interface Props { }
+
+export const Rooms = (props: Props) => {
+    const { } = props
+
+    return (
+        <RoomsSvg></RoomsSvg>
+    )
+}
--- room_reservation_group_work/src/modules/rooms/components/index.tsx
+++ room_reservation_group_work/src/modules/rooms/components/index.tsx
@@ -1,1 +1,11 @@
 import React from 'react'
+import { Rooms } from './Rooms'
+
+interface Props { }
+export const RoomsPage = (props: Props) => {
+    const { } = props
+
+    return (
+        <Rooms> </Rooms>
+    )
+}
